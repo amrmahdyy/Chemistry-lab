9317c12e44abc015d3bf8aeb140bbadbd1efe0e6
amrmahdyy <amrmahdy2@gmail.com>
2021-11-08 18:20:22 +0200

    added dropdown functionality in index.js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".sidebar .nav-link").forEach(function (element) {
-    element.addEventListener("click", function (e) {
-      let nextEl = element.nextElementSibling;
-      let parentEl = element.parentElement;
+// navbar dropdown
+const dropdownAnchors = document.querySelectorAll(".nav-item.dropdown");
+// console.log(dropdownAnchors[0].children[0]);
 
-      if (nextEl) {
-        e.preventDefault();
-        let mycollapse = new bootstrap.Collapse(nextEl);
+const mediaQuery = window.matchMedia("(max-width: 992px)");
 
-        if (nextEl.classList.contains("show")) {
-          mycollapse.hide();
-        } else {
-          mycollapse.show();
-          // find other submenus with class=show
-          var opened_submenu =
-            parentEl.parentElement.querySelector(".submenu.show");
-          // if it exists, then close all of them
-          if (opened_submenu) {
-            new bootstrap.Collapse(opened_submenu);
-          }
-        }
-      }
-    }); // addEventListener
-  }); // forEach
-});
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    // Then log the following message to the console
+    console.log("Media Query Matched!");
+    console.log(
+      dropdownAnchors[0].children[1].children[0].children[0].getAttribute(
+        "href"
+      )
+    );
+    dropdownAnchors.forEach((navLink) => {
+      navLink.children[0].setAttribute("href", "#");
+      navLink.children[0].setAttribute("data-bs-toggle", "dropdown");
+    });
+  } else {
+    dropdownAnchors.forEach((navLink) => {
+      const urlNav =
+        navLink.children[1].children[0].children[0].getAttribute("href");
+      navLink.children[0].setAttribute("href", urlNav);
+      navLink.children[0].removeAttribute("data-bs-toggle");
+    });
+  }
+}
+
+// Register event listener
+mediaQuery.addListener(handleTabletChange);
+
+// Initial check
+handleTabletChange(mediaQuery);
 // DOMContentLoaded  end
